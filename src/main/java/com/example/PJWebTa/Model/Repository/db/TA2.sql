59e422bf4db6ea1c4666baba1ca95f5bffbf469c
Trung-Be-Ac <ahoangtrung12345@gmail.com>
2025-05-16 18:46:06 +0700

    up

--- src/main/java/com/example/PJWebTa/Model/Repository/db/TA2.sql
+++ src/main/java/com/example/PJWebTa/Model/Repository/db/TA2.sql
@@ -27,7 +27,7 @@ values
 ('Phạm Tạ D', 'PTD@gmail.com', 1, '2024-01-04', 'student', false, '.....jpg', 'acc4', 'pass4'),
 ('Đô Thành C', 'DTC@gmail.com', 4, '2024-01-03', 'teacher', true, '.....jpg', 'acc3', 'pass3'),
 ('Phan Mạnh E', 'PME@gmail.com', 2, '2024-01-05', 'student', true, '.....jpg', 'acc5', 'pass5');
--- --------------------
+-- --------------------dddddddddddddddddd
 
 create table course(
 course_id int primary key auto_increment,
